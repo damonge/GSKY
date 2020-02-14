3456ae150969a0963842f1d3cb998bda4e7f86d1
David Alonso <dam_phys@gmail.com>
2020-02-14 11:43:23 -0800

    updated query scripts

--- data_query/get_data.py
+++ data_query/get_data.py
@@ -10,15 +10,15 @@ import os
 ################################
 
 #Per-frame metadata
-write_frames("s16a_wide","frames_wide.sql",submit=True)
-write_frames("s16a_deep","frames_deep.sql",submit=True)
-write_frames("s16a_udeep","frames_udeep.sql",submit=True)
+#write_frames("s16a_wide","frames_wide.sql",submit=True)
+#write_frames("s16a_deep","frames_deep.sql",submit=True)
+#write_frames("s16a_udeep","frames_udeep.sql",submit=True)
 
-'''
 #WIDE fields
-for fld in ['aegis','gama09h','gama15h','hectomap','wide12h','xmm_lss'] :
-    write_fieldsearch("pdr1_wide",fld,"field_wide_"+fld+"_pz_strict.sql",do_field=True,
-                      submit=True,do_photoz=True,strict_cuts=True)
+for fld in ['aegis','gama09h','gama15h','hectomap','wide01h','wide12h','xmm']:
+    write_fieldsearch("s16a_wide",fld,"field_wide_"+fld+"_pz_strict.sql",
+                      submit=True, strict_cuts=True, do_download=False)
+'''
 write_fieldsearch("pdr1_wide",'vvds',"field_wide_vvds_h1_pz_strict.sql",do_field=True,
                   submit=True,ra_range=[330.,336.],do_photoz=True,strict_cuts=True,part=1)
 write_fieldsearch("pdr1_wide",'vvds',"field_wide_vvds_h2_pz_strict.sql",do_field=True,
--- data_query/hsc_queries.py
+++ data_query/hsc_queries.py
@@ -9,6 +9,7 @@ def submit_job(fname_sql,output_format,do_preview=False,do_download=True,output_
         return
 
     command="python hscReleaseQuery.py --user=damonge@local"
+    command+=" -r pdr1 "
     if do_preview :
         command+=" -p"
     command+=" -f "+output_format
@@ -45,12 +46,13 @@ def write_frames(tablename,fname_out,output_format='fits',submit=False,do_downlo
     if submit :
         submit_job(fname_out,output_format,do_download=do_download,output_file=fname_job)
 
-def write_fieldsearch(tablename,fieldname,fname_out,output_format="fits",do_field=True,submit=False,ra_range=None,
-                      do_photoz=False,strict_cuts=False,do_download=True,part=None) :
+def write_fieldsearch(tablename,fieldname,fname_out,output_format="fits",
+                      submit=False,ra_range=None,exhaustive=False,
+                      strict_cuts=False,do_download=True,part=None) :
     filters=['g','r','i','z','y']
     stout="-- Run field, "+fname_out+"\n"
 
-    fname_job=None
+    fname_job='none'
     if do_download :
         fname_job=prd.predir_saving+tablename.upper()+"_"+fieldname.replace('_','').upper()
         if part is not None :
@@ -63,108 +65,114 @@ def write_fieldsearch(tablename,fieldname,fname_out,output_format="fits",do_fiel
         sthere=""
         for fi in filters :
             if behind :
-                sthere+="       ,forced."+name+fi+"\n"
+                sthere+="       ,a."+name+fi+"\n"
             else :
-                sthere+="       ,forced."+fi+name+"\n"
+                sthere+="       ,a."+fi+name+"\n"
         return sthere
 
     stout+="SELECT object_id\n"
-    stout+="       ,forced.ra as ra\n"
-    stout+="       ,forced.dec as dec\n"
-    stout+="       ,forced.tract as tract\n"
-    stout+="       ,forced.patch as patch\n"
+    stout+="       ,a.ra as ra\n"
+    stout+="       ,a.dec as dec\n"
+    stout+="       ,a.tract as tract\n"
+    stout+="       ,a.patch as patch\n"
     stout+=add_filters("merge_peak_")
     stout+=add_filters("countinputs",behind=False)
-    stout+="       ,forced.iflags_pixel_bright_object_center\n"
-    stout+="       ,forced.iflags_pixel_bright_object_any\n" 
-    stout+="       ,forced.iclassification_extendedness\n"
-    stout+="       ,meas.iblendedness_abs_flux as iblendedness_abs_flux\n"
+    stout+="       ,a.iflags_pixel_bright_object_center\n"
+    stout+="       ,a.iflags_pixel_bright_object_any\n" 
+    stout+="       ,a.iclassification_extendedness\n"
+    stout+="       ,b.iblendedness_abs_flux as iblendedness_abs_flux\n"
     #Dust extinction
     stout+=add_filters("a_")
-    #Psf fluxes and magnitudes
-    stout+=add_filters("flux_psf",behind=False)
-    stout+=add_filters("flux_psf_err",behind=False)
-    stout+=add_filters("flux_psf_flags",behind=False)
-    stout+=add_filters("mag_psf",behind=False)
-    stout+=add_filters("mag_psf_err",behind=False)
-    #Aperture fluxes and magnitudes
-    stout+=add_filters("flux_aperture10",behind=False)
-    stout+=add_filters("flux_aperture10_err",behind=False)
-    stout+=add_filters("flux_aperture_flags",behind=False)
-    stout+=add_filters("mag_aperture10",behind=False)
-    stout+=add_filters("mag_aperture10_err",behind=False)
+    if exhaustive:
+        #Psf fluxes and magnitudes
+        stout+=add_filters("flux_psf",behind=False)
+        stout+=add_filters("flux_psf_err",behind=False)
+        stout+=add_filters("flux_psf_flags",behind=False)
+        stout+=add_filters("mag_psf",behind=False)
+        stout+=add_filters("mag_psf_err",behind=False)
+        #Aperture fluxes and magnitudes
+        stout+=add_filters("flux_aperture10",behind=False)
+        stout+=add_filters("flux_aperture10_err",behind=False)
+        stout+=add_filters("flux_aperture_flags",behind=False)
+        stout+=add_filters("mag_aperture10",behind=False)
+        stout+=add_filters("mag_aperture10_err",behind=False)
     #Cmodel fluxes and magnitudes
     stout+=add_filters("cmodel_flux",behind=False)
     stout+=add_filters("cmodel_flux_err",behind=False)
     stout+=add_filters("cmodel_flux_flags",behind=False)
     stout+=add_filters("cmodel_mag",behind=False)
     stout+=add_filters("cmodel_mag_err",behind=False)
-    if do_photoz :
-        stout+=add_photoz("eab")
-        stout+=add_photoz("frz")
-        stout+=add_photoz("nnz")
+    stout+=add_photoz("eab")
+    stout+="       ,c.ishape_hsm_regauss_e1 as ishape_hsm_regauss_e1\n"
+    stout+="       ,c.ishape_hsm_regauss_e2 as ishape_hsm_regauss_e2\n"
+    stout+="       ,c.ishape_hsm_regauss_sigma as ishape_hsm_regauss_sigma\n"
+    stout+="       ,c.ishape_hsm_regauss_resolution as ishape_hsm_regauss_resolution\n"
+    stout+="       ,d.ishape_hsm_regauss_derived_shape_weight as ishape_hsm_regauss_derived_shape_weight\n"
+    stout+="       ,d.ishape_hsm_regauss_derived_shear_bias_m as ishape_hsm_regauss_derived_shear_bias_m\n"
+    stout+="       ,d.ishape_hsm_regauss_derived_shear_bias_c1 as ishape_hsm_regauss_derived_shear_bias_c1\n"
+    stout+="       ,d.ishape_hsm_regauss_derived_shear_bias_c2 as ishape_hsm_regauss_derived_shear_bias_c2\n"
+    stout+="       ,d.ishape_hsm_regauss_derived_sigma_e as ishape_hsm_regauss_derived_sigma_e\n"
+    stout+="       ,d.ishape_hsm_regauss_derived_rms_e as ishape_hsm_regauss_derived_rms_e\n"    
     stout+="FROM\n"
-    stout+="       "+tablename+".forced as forced\n"
-    stout+="       LEFT JOIN "+tablename+".meas meas USING (object_id)\n"
-    if do_photoz :
-        stout+="       LEFT JOIN "+tablename+".photoz_ephor_ab peab USING (object_id)\n"
-        stout+="       LEFT JOIN "+tablename+".photoz_frankenz pfrz USING (object_id)\n"
-        stout+="       LEFT JOIN "+tablename+".photoz_nnpz pnnz USING (object_id)\n"
+    stout+="       "+tablename+".forced as a\n"
+    stout+="       LEFT JOIN "+tablename+".meas b USING (object_id)\n"
+    stout+="       LEFT JOIN "+tablename+".meas2 c USING (object_id)\n"
+    stout+="       LEFT JOIN "+tablename+".photoz_ephor_ab peab USING (object_id)\n"
+    stout+="       LEFT JOIN "+tablename+".weaklensing_hsm_regauss d USING (object_id)\n"
     stout+="WHERE\n"
-    stout+="       forced.detect_is_primary=True and\n"
-    stout+="       forced.icmodel_flags_badcentroid=False and\n"
-    stout+="       forced.icentroid_sdss_flags=False and\n"
-    stout+="       forced.iflags_pixel_edge=False and\n"
-    stout+="       forced.iflags_pixel_interpolated_center=False and\n"
-    stout+="       forced.iflags_pixel_saturated_center=False and\n"
-    stout+="       forced.iflags_pixel_cr_center=False and\n"
-    stout+="       forced.iflags_pixel_bad=False and\n"
-    stout+="       forced.iflags_pixel_suspect_center=False and\n"
-    stout+="       forced.iflags_pixel_clipped_any=False and\n"
-    stout+="       meas.ideblend_skipped=False"
+    stout+="       a.detect_is_primary=True and\n"
+    stout+="       a.icmodel_flags_badcentroid=False and\n"
+    stout+="       a.icentroid_sdss_flags=False and\n"
+    stout+="       a.iflags_pixel_edge=False and\n"
+    stout+="       a.iflags_pixel_interpolated_center=False and\n"
+    stout+="       a.iflags_pixel_saturated_center=False and\n"
+    stout+="       a.iflags_pixel_cr_center=False and\n"
+    stout+="       a.iflags_pixel_bad=False and\n"
+    stout+="       a.iflags_pixel_suspect_center=False and\n"
+    stout+="       a.iflags_pixel_clipped_any=False and\n"
+    stout+="       b.ideblend_skipped=False"
     if strict_cuts :
         stout+=" and\n"
-        stout+="       forced.gcentroid_sdss_flags=False and\n"
-        stout+="       forced.rcentroid_sdss_flags=False and\n"
-        stout+="       forced.zcentroid_sdss_flags=False and\n"
-        stout+="       forced.ycentroid_sdss_flags=False and\n"
-        stout+="       forced.gcmodel_flux_flags=False and\n"
-        stout+="       forced.rcmodel_flux_flags=False and\n"
-        stout+="       forced.icmodel_flux_flags=False and\n"
-        stout+="       forced.zcmodel_flux_flags=False and\n"
-        stout+="       forced.ycmodel_flux_flags=False and\n"
-        stout+="       forced.gflux_psf_flags=False and\n"
-        stout+="       forced.rflux_psf_flags=False and\n"
-        stout+="       forced.iflux_psf_flags=False and\n"
-        stout+="       forced.zflux_psf_flags=False and\n"
-        stout+="       forced.yflux_psf_flags=False and\n"
-        stout+="       forced.gflags_pixel_edge=False and\n"
-        stout+="       forced.rflags_pixel_edge=False and\n"
-        stout+="       forced.zflags_pixel_edge=False and\n"
-        stout+="       forced.yflags_pixel_edge=False and\n"
-        stout+="       forced.gflags_pixel_interpolated_center=False and\n"
-        stout+="       forced.rflags_pixel_interpolated_center=False and\n"
-        stout+="       forced.zflags_pixel_interpolated_center=False and\n"
-        stout+="       forced.yflags_pixel_interpolated_center=False and\n"
-        stout+="       forced.gflags_pixel_saturated_center=False and\n"
-        stout+="       forced.rflags_pixel_saturated_center=False and\n"
-        stout+="       forced.zflags_pixel_saturated_center=False and\n"
-        stout+="       forced.yflags_pixel_saturated_center=False and\n"
-        stout+="       forced.gflags_pixel_cr_center=False and\n"
-        stout+="       forced.rflags_pixel_cr_center=False and\n"
-        stout+="       forced.zflags_pixel_cr_center=False and\n"
-        stout+="       forced.yflags_pixel_cr_center=False and\n"
-        stout+="       forced.gflags_pixel_bad=False and\n"
-        stout+="       forced.rflags_pixel_bad=False and\n"
-        stout+="       forced.zflags_pixel_bad=False and\n"
-        stout+="       forced.yflags_pixel_bad=False\n"
-    if do_field :
-        stout+=" and\n"
-        stout+="       "+tablename+".search_"+fieldname+"(object_id)"
+        stout+="       a.gcentroid_sdss_flags=False and\n"
+        stout+="       a.rcentroid_sdss_flags=False and\n"
+        stout+="       a.zcentroid_sdss_flags=False and\n"
+        stout+="       a.ycentroid_sdss_flags=False and\n"
+        stout+="       a.gcmodel_flux_flags=False and\n"
+        stout+="       a.rcmodel_flux_flags=False and\n"
+        stout+="       a.icmodel_flux_flags=False and\n"
+        stout+="       a.zcmodel_flux_flags=False and\n"
+        stout+="       a.ycmodel_flux_flags=False and\n"
+        stout+="       a.gflux_psf_flags=False and\n"
+        stout+="       a.rflux_psf_flags=False and\n"
+        stout+="       a.iflux_psf_flags=False and\n"
+        stout+="       a.zflux_psf_flags=False and\n"
+        stout+="       a.yflux_psf_flags=False and\n"
+        stout+="       a.gflags_pixel_edge=False and\n"
+        stout+="       a.rflags_pixel_edge=False and\n"
+        stout+="       a.zflags_pixel_edge=False and\n"
+        stout+="       a.yflags_pixel_edge=False and\n"
+        stout+="       a.gflags_pixel_interpolated_center=False and\n"
+        stout+="       a.rflags_pixel_interpolated_center=False and\n"
+        stout+="       a.zflags_pixel_interpolated_center=False and\n"
+        stout+="       a.yflags_pixel_interpolated_center=False and\n"
+        stout+="       a.gflags_pixel_saturated_center=False and\n"
+        stout+="       a.rflags_pixel_saturated_center=False and\n"
+        stout+="       a.zflags_pixel_saturated_center=False and\n"
+        stout+="       a.yflags_pixel_saturated_center=False and\n"
+        stout+="       a.gflags_pixel_cr_center=False and\n"
+        stout+="       a.rflags_pixel_cr_center=False and\n"
+        stout+="       a.zflags_pixel_cr_center=False and\n"
+        stout+="       a.yflags_pixel_cr_center=False and\n"
+        stout+="       a.gflags_pixel_bad=False and\n"
+        stout+="       a.rflags_pixel_bad=False and\n"
+        stout+="       a.zflags_pixel_bad=False and\n"
+        stout+="       a.yflags_pixel_bad=False\n"
+    stout+=" and\n"
+    stout+="       "+tablename+".search_"+fieldname+"(a.skymap_id)"
     if ra_range is not None :
         stout+=" and\n"
-        stout+="       forced.ra>=%.3lf and\n"%(ra_range[0])
-        stout+="       forced.ra<%.3lf"%(ra_range[1])
+        stout+="       a.ra>=%.3lf and\n"%(ra_range[0])
+        stout+="       a.ra<%.3lf"%(ra_range[1])
     stout+="\n;\n"
 
     f=open(fname_out,"w")
