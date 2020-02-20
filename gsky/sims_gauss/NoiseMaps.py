b54a7e9077bb0b9dfb8e1ebb2a60c179e1d08b73
Andrina Nicola <aaxnicola@gmail.com>
2020-02-20 12:31:03 +0000

    Bugfix.

--- gsky/sims_gauss/NoiseMaps.py
+++ gsky/sims_gauss/NoiseMaps.py
@@ -235,7 +235,7 @@ class NoiseMaps(object):
                     data[probe]['fsk'] = fsk
                     if self.params['posfromshearcat'] == 0:
                         assert 'path2shearmask' in self.params, 'Requesting randomized galaxy positions for gamma but path2shearmask not provided. Aborting.'
-                        tempmap = read_flat_map(self.params['path2shearmask'])
+                        tempmap = read_flat_map(self.params['path2shearmask'], i_map=3)
                         data[probe]['shearmask'] = tempmap
 
                 elif probe == 'deltag':
