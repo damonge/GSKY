c8fd92634b73264b0d39572f9832e2dada17470a
Andrina Nicola <aaxnicola@gmail.com>
2020-02-21 13:06:20 +0000

    Bugfix.

--- gsky/noise_mocks.py
+++ gsky/noise_mocks.py
@@ -70,7 +70,7 @@ class NoiseMocks(PipelineStage) :
         noiseparams_keys = ['probes', 'noisemodel', 'posfromshearcat']
         noiseparams = {key: self.config[key] for key in noiseparams_keys}
         noiseparams['path2shearcat'] = self.get_input('calib_catalog')
-        noiseparams['path2fsk'] = self.get_output('masked_fraction')
+        noiseparams['path2fsk'] = self.get_input('masked_fraction')
         simparams_keys = ['probes', 'spins', 'path2cls', 'nrealiz', 'ell_bpws', 'pixwindow', 'nell_theor']
         simparams = {key: self.config[key] for key in simparams_keys}
 
