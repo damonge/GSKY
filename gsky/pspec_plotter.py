3d5f33e3a9bdf19c8b12de4d4741c59de644b853
Andrina Nicola <aaxnicola@gmail.com>
2020-03-05 15:59:03 -0500

    Bugfix.

--- gsky/pspec_plotter.py
+++ gsky/pspec_plotter.py
@@ -16,8 +16,8 @@ class PSpecPlotter(PipelineStage) :
     name="PSpecPlotter"
     inputs=[]
     outputs=[]
-    config_options={'path2saccs': list, 'output_run_dir': 'NONE', 'output_plot_dir': 'NONE',
-                    'fig_name': str, 'tracers': list, 'plot_comb': 'all', 'cl_type': 'cl_ee', 'plot_errors': False,
+    config_options={'path2saccs': [str], 'output_run_dir': 'NONE', 'output_plot_dir': 'NONE',
+                    'fig_name': str, 'tracers': [str], 'plot_comb': 'all', 'cl_type': 'cl_ee', 'plot_errors': False,
                     'plot_theory': False, 'weightpow': 2, 'logscale_x': False, 'logscale_y': False}
 
     def get_output_fname(self,name,ext=None):
