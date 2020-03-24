5cf297f5240fcadbdde7e837f3e7e9faee0dbfd1
Andrina Nicola <aaxnicola@gmail.com>
2020-03-24 17:30:27 -0400

    Documentation of prefactor.

--- theory/SZ_theory.py
+++ theory/SZ_theory.py
@@ -140,6 +140,16 @@ class SZTracer(ccl.Tracer):
         a_arr = ccl.scale_factor_of_chi(cosmo, chi_arr) 
         # avoid recomputing every time
         # Units of eV * Mpc / cm^3
+
+        # sigma_T = 6.65e-29 m2
+        # m_e = 9.11e-31 kg
+        # c = 3e8  m/s
+
+        # eV2J = 1.6e-19 eV/J (J=kg m2/s2)
+        # cm2pc = 3.1e18 cm/pc
+
+        # prefac = (sigma_t*(10**2)**2/(m_e*c**2/J2eV))*cm2pc*10**6
+
         prefac = 4.017100792437957e-06
         w_arr = prefac * a_arr
 
