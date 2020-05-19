12c6ce4d2c7f1b53be403a1c133fe561336cc966
Andrina Nicola <aaxnicola@gmail.com>
2020-05-19 12:38:08 -0400

    Bugfix.

--- theory/SZ_theory.py
+++ theory/SZ_theory.py
@@ -141,7 +141,7 @@ class HaloProfileBattaglia(ccl.halos.HaloProfile):
                 self.xuse = copy.deepcopy(self.xarr)
             kR_use = copy.deepcopy(kR)
 
-        integ = self.xuse**2*self.sinc_interp(kR_use*self.xuse)*ff
+        integ = self.xuse**2*self._sinc_interp(kR_use*self.xuse)*ff
 
         fourier_prof = np.trapz(integ, self.xuse, axis=-1)
 
