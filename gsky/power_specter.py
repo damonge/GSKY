c9d72351cf1aee96002cb2d5113214f2f7b08454
dalal <dalal@lorentz.leidenuniv.nl>
2022-01-07 10:39:20 -0700

    masking low nexp

--- gsky/power_specter.py
+++ gsky/power_specter.py
@@ -692,8 +692,8 @@ class PowerSpecter(PipelineStage) :
             #Read systematics cut data
             data_syst=np.genfromtxt(self.get_input('syst_masking_file'),
                                     dtype=[('name','|U32'),('band','|U4'),('gl','|U4'),('thr','<f8')])
+            print(data_syst)
             for d in data_syst :
-                print(data_syst)
                 #Read systematic
                 if d['name'].startswith('oc_'):
                     sysmap=self.read_map_bands(self.get_input(d['name'][3:]+'_maps'),False,d['band'],
