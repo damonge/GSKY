9f9c4cb1c383667891b57a266312a22b66fa7d03
dalal <dalal@lorentz.leidenuniv.nl>
2022-01-07 13:29:48 -0700

    masking low nexp

--- gsky/power_specter.py
+++ gsky/power_specter.py
@@ -690,10 +690,10 @@ class PowerSpecter(PipelineStage) :
             #Mask systematics
             msk_syst=msk_bi.copy()
             #Read systematics cut data
-            #Roohi change data_syst format
-            data_syst1=np.genfromtxt(self.get_input('syst_masking_file'),
+            #Roohi: add dummy row to file
+            data_syst=np.genfromtxt(self.get_input('syst_masking_file'),
                                     dtype=[('name','|U32'),('band','|U4'),('gl','|U4'),('thr','<f8')])
-            data_syst=[data_syst1]
+            print(len(data_syst))
             for d in data_syst :
                 #Read systematic
                 if d['name'].startswith('oc_'):
