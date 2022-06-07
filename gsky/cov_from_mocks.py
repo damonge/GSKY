2ff7a652dcf6a0d5491130497387e534b06064b5
dalal <dalal@lorentz.leidenuniv.nl>
2022-06-07 19:05:02 -0400

    covariance from mocks

--- gsky/cov_from_mocks.py
+++ gsky/cov_from_mocks.py
@@ -206,7 +206,7 @@ class CovFromMocks(object):
           'mocks_dir': '/projects/HSC/weaklens/xlshare/S19ACatalogs/catalog_mock/fields/XMM/'}
 
         n_realizations = len(os.listdir(config['mocks_dir']))
-        n_realizations = 8
+        n_realizations = 1
         realizations = np.arange(n_realizations)
         ncpus = multiprocessing.cpu_count()
         ncpus = 1
