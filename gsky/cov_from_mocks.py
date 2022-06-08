4b3eaf8c1cae892bc53ca5800b2af4412aa69f4f
dalal <dalal@lorentz.leidenuniv.nl>
2022-06-08 12:14:55 -0400

    covariance from mocks

--- gsky/cov_from_mocks.py
+++ gsky/cov_from_mocks.py
@@ -217,8 +217,8 @@ class CovFromMocks(object):
     def go(self):
         config={'plots_dir': None,
           'min_snr': 10., 'depth_cut': 24.5,
-          'mapping': {'wcs': None, 'res': 0.0285,
-                      'res_bo': 0.003, 'pad': 0.1,
+          'mapping': {'wcs': None, 'res': 0.01666666666667,
+                      'res_bo': 0.003, 'pad': 0.2,
                       'projection': 'CAR'},
           'band': 'i', 'depth_method': 'fluxerr',
           'shearrot': 'noflip', 'mask_type': 'sirius',
@@ -227,8 +227,7 @@ class CovFromMocks(object):
           'pz_bins': [0.3, 0.6, 0.9, 1.2, 1.5],
           'nz_bin_num': 100,
           'nz_bin_max': 4.0,
-          'shearrot': 'noflip',
-          'ra':  'ra_mock', 'dec':  'dec_mock', 'shape_noise': True,
+          'shape_noise': True,
           'mocks_dir': '/projects/HSC/weaklens/xlshare/S19ACatalogs/catalog_mock/fields/XMM/'}
 
         n_realizations = len(os.listdir(config['mocks_dir']))
@@ -251,8 +250,8 @@ class CovFromMocks(object):
     def __call__(self, realization):
         config={'plots_dir': None,
           'min_snr': 10., 'depth_cut': 24.5,
-          'mapping': {'wcs': None, 'res': 0.0285,
-                      'res_bo': 0.003, 'pad': 0.1,
+          'mapping': {'wcs': None, 'res': 0.01666666666667,
+                      'res_bo': 0.003, 'pad': 0.2,
                       'projection': 'CAR'},
           'band': 'i', 'depth_method': 'fluxerr',
           'shearrot': 'noflip', 'mask_type': 'sirius',
@@ -261,8 +260,7 @@ class CovFromMocks(object):
           'pz_bins': [0.3, 0.6, 0.9, 1.2, 1.5],
           'nz_bin_num': 100,
           'nz_bin_max': 4.0,
-          'shearrot': 'noflip',
-          'ra':  'ra_mock', 'dec':  'dec_mock', 'shape_noise': True,
+          'shape_noise': True,
           'mocks_dir': '/projects/HSC/weaklens/xlshare/S19ACatalogs/catalog_mock/fields/XMM/'}
         logger.info('Running realization : {}.'.format(realization))
         band = config['band']
