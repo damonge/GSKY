c92b116d0c43b4f9e8f0dac31ca869dc3cb94f4a
dalal <dalal@lorentz.leidenuniv.nl>
2022-09-16 12:19:08 -0400

    redshift independet PSF systematics

--- gsky/shear_mapper.py
+++ gsky/shear_mapper.py
@@ -372,7 +372,9 @@ class ShearMapper(PipelineStage):
         header = self.fsk.wcs.to_header()
         hdus = []
         shp_mp = [self.fsk.ny, self.fsk.nx]
+        logger.info(gammamap_allz.shape)
         for im, m_list in enumerate(gammamap_allz):
+            logger.info(im)
             if im == len(gammamap_allz) - 1:
                 bin_tag = 'all'
             else:
