644aba7c7d46bc16c68e28ff76f5e3689f240f7f
dalal <dalal@lorentz.leidenuniv.nl>
2023-02-13 15:59:50 -0500

    no tomography power spectra

--- gsky/cov_allz_from_mocks.py
+++ gsky/cov_allz_from_mocks.py
@@ -366,7 +366,7 @@ class CovAllzFromMocks(object):
           'shape_noise': True,
           'rm_gama09h_region': True,
           'mocks_dir': '/projects/HSC/weaklens/xlshare/S19ACatalogs/catalog_mock/fields/GAMA15H/',
-          'clean_catalog_data': '/tigress/rdalal/fourier_space_shear/GSKY_outputs/GAMA15H_ceci/catalog1/clean_catalog.fits',
+          'clean_catalog_data': '/tigress/rdalal/fourier_space_shear/GSKY_outputs/GAMA15H_ceci/catalog2/clean_catalog.fits',
           'mock_correction_factors': '/tigress/rdalal/fourier_space_shear/mocks_correction_factor.npy'}
 
         n_realizations = len(os.listdir(config['mocks_dir']))
@@ -408,7 +408,7 @@ class CovAllzFromMocks(object):
           'rm_gama09h_region': True,
           'mocks_dir': '/projects/HSC/weaklens/xlshare/S19ACatalogs/catalog_mock/fields/GAMA15H/',
           'selection_array': '/projects/HSC/weaklens/xlshare/S19ACatalogs/photoz_2pt/fiducial_dnnzbin_w95c027/source_sel_GAMA15H.fits',
-          'clean_catalog_data': '/tigress/rdalal/fourier_space_shear/GSKY_outputs/GAMA15H_ceci/catalog1/clean_catalog.fits',
+          'clean_catalog_data': '/tigress/rdalal/fourier_space_shear/GSKY_outputs/GAMA15H_ceci/catalog2/clean_catalog.fits',
           'mock_correction_factors': '/tigress/rdalal/fourier_space_shear/mocks_correction_factor.npy'}
         logger.info('Running realization : {}.'.format(realization))
         band = config['band']
